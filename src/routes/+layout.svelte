779bf0fa1712b376fd96ba5030d0c44657a0de8f
Gil <friedmang@bxscience.edu>
2022-11-27 18:37:26 -0500

    fr this time

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css";
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Header/>
+  
+  <div style="position:relative; z-index: 0; min-height: 80vh;">
+  <slot/>
+  </div>
+
+<Footer/>
